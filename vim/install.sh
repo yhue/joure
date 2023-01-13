05dd317882594528f7ec74fa649bd97c2cf387eb
yhue <yhue@ipcyron.com>
2023-01-13 11:50:45 +0900

    add: vim space

--- vim/install.sh
+++ vim/install.sh
@@ -11,7 +11,7 @@ mkdir -p ${orgdir}
 # install peaberry
 [[ -d /git/peaberry ]] && rm -rf /git/peaberry
 git clone https://github.com/yhue/peaberry.git /git/peaberry
-ln -s /git/peaberry ~/.vim
+ln -s /git/peaberry/vim ~/.vim
 chmod 755 ~/.vim && chmod 700 ~/.vim/trace
 
 # install vim-plug
