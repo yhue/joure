b9716e84b89b10004a2536f4f1a35104ca9bc7c2
yhue <yhue@ipcyron.com>
2023-03-19 18:35:54 +0900

    modify: naming SCRIPT_DIR to dir

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 # reset peaberry
 peadir=~/.peaberry
 [[ -d ${peadir} ]] && rm -rf ${peadir}
 git clone https://github.com/yhue/peaberry.git ${peadir}
 
-. ${SCRIPT_DIR}/vim/setup.sh
-. ${SCRIPT_DIR}/git/setup.sh
+. ${dir}/vim/setup.sh
+. ${dir}/git/setup.sh
